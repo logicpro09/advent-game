8e4837dbbe9dea0dd9bbd6bd6677bb43424b184c
Jonathan Luu <jonluu1@gmail.com>
2020-12-14 17:11:19 -0800

    Added comments to Item object

--- Item.java
+++ Item.java
@@ -1,9 +1,12 @@
+// jpluu
+// Item object
 public class Item {
     public String name;
     public int quantity;
     public boolean consumable;
     public String description;
 
+    // constructors
     public Item() {
         this.name = "null";
         this.quantity = -1;
@@ -16,6 +19,7 @@ public class Item {
         this.consumable = false;
     }
 
+    // getters
     public String getName() {
         return name;
     }
@@ -28,6 +32,7 @@ public class Item {
         return consumable;
     }
 
+    // setters
     public void setName(String name) {
         this.name = name;
     }
@@ -40,9 +45,14 @@ public class Item {
         this.consumable = consumable;
     }
 
+    // helpers
+    // add - when refilling items at the shop
     public void add(int amount) {
         this.quantity += amount;
     }
+
+    // use - if an item can be use and has quantity left
+    // returns a boolean if the item is used
     public boolean use() {
         if (consumable && this.quantity > 0) {
             this.quantity -= 1;
